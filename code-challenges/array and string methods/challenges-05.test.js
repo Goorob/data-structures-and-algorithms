1304eb8998c65d187e2ce78ca84212e55c9efcf1
Goorob <orobalswalqah@gmail.com>
2019-11-17 00:49:46 +0200

    challenge 2

--- code-challenges/array and string methods/challenges-05.test.js
+++ code-challenges/array and string methods/challenges-05.test.js
@@ -31,7 +31,7 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  return arr.split('');
 };
 
 

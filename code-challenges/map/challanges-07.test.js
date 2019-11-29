620a1e50fe195096776a978f533e799819dfe267
Goorob <orobalswalqah@gmail.com>
2019-11-29 20:42:03 +0200

    challange 1 solved

--- code-challenges/map/challanges-07.test.js
+++ code-challenges/map/challanges-07.test.js
@@ -10,9 +10,13 @@ You may choose to complete this challenge using a for loop, for...in syntax, or 
 For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
 ------------------------------------------------------------------------------------------------ */
 
-const forLoopTwoToThe = (arr) => {
-  // Solution code here...
-};
+const forLoopTwoToThe = (array) => {
+ 
+  let arr = [];
+    for (let val of array) {arr.push(Math.pow(2, val));}
+    return arr;
+  };
+  
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2

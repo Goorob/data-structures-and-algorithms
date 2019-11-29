deb31d3ab991c86f8793e3a800ceb7f7d346539f
Goorob <orobalswalqah@gmail.com>
2019-11-30 01:41:43 +0200

    challange 2 solved

--- code-challenges/map/challanges-07.test.js
+++ code-challenges/map/challanges-07.test.js
@@ -24,9 +24,14 @@ CHALLENGE 2
 Write a function named forEachTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1, but uses forEach instead of a for loop.
 ------------------------------------------------------------------------------------------------ */
 
-const forEachTwoToThe = (arr) => {
-  // Solution code here...
+const forEachTwoToThe = (array) => {
+  const arr = [];
+  array.forEach(val => {
+   arr.push(Math.pow(2, val));
+  });
+  return arr;
 };
+  
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3

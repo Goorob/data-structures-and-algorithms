c7bcf68491200e0297ac776836459c17038b565f
Goorob <orobalswalqah@gmail.com>
2019-11-30 21:57:35 +0200

    challenge 3 solved

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -40,7 +40,9 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+
+    return arr.filter(val => !forbiddenValues.includes(val));
+    
 };
 
 /* ------------------------------------------------------------------------------------------------
